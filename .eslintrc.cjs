4798e568e25c1f537542212b0025e23e5af40808
hosoyan <78296405+linked34ce@users.noreply.github.com>
2024-04-22 01:01:11 +0900

    setup environment and create pipeline
    * use vue.js for single page application
    
    * install packages
    
    * try github actions
    
    * use actions/checkout
    
    * use tailwind for css framework
    
    * set several coding rules
    
    * create easy pipeline
    
    * run unit test in pipeline
    
    * create pipeline
    
    * change job names
    
    * shorten pipeline
    
    * create config for deploying on github pages
    
    * auto deploy when pushing to main
    
    * enable package cache
    
    * use latest actions
    
    * refactor yaml files

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'prettier'
+  ],
+  overrides: [
+    {
+      files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:playwright/recommended']
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    semi: ['error', 'always'],
+    'semi-spacing': ['error', { after: true, before: false }],
+    'semi-style': ['error', 'last'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error'
+  }
+};
