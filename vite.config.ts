6e286eee75d3111d5eb76831ecbbbb762bad7529
hosoyan <78296405+linked34ce@users.noreply.github.com>
2024-04-22 02:57:05 +0900

    change vite config to deploy vue app
    * build vue on github pages
    
    * set playwright base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import vue from '@vitejs/plugin-vue';
 import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue(), VueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: mode === 'production' ? '/nijigasaki-introduction/' : '/'
+  };
 });
