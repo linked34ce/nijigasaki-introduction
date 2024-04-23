e8ff4b4bd343c6ce60e0264715b21e34f2899b85
hosoyan <78296405+linked34ce@users.noreply.github.com>
2024-04-24 03:03:59 +0900

    introduce daisyui and create moving background
    * install daisyui and build tailwind
    
    * create moving background
    
    * fix e2e test
    
    * install cssnano

--- src/components/BackGround.vue
+++ src/components/BackGround.vue
@@ -30,8 +30,6 @@ const backGroundClassName = computed(() => {
 }
 
 .area {
-  // background: #4e54c8;
-  // background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
   width: 100%;
   height: 100vh;
   position: absolute;
