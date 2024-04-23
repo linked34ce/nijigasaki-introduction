1e1c4cdfa0f420a9a3b8fac3c4c7e64cbac7f35f
hosoyan <9797ouki@gmail.com>
2024-04-24 02:52:56 +0900

    fix e2e test

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -5,5 +5,5 @@ import { test, expect } from '@playwright/test';
 
 test('visits the app root url', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('button')).toHaveText('Primary');
+  await expect(await page.locator('.circle').all()).toHaveLength(10);
 });
