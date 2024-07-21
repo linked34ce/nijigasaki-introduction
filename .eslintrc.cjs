70f9d9c5d5c399e6af7f59af7d520bd32c4a9118
hosoyan <78296405+linked34ce@users.noreply.github.com>
2024-07-21 13:35:23 +0900

    add git hooks and write e2e
    * change public directory when deployed
    
    * add simple-git-hooks
    
    * test pre-push
    
    * automatically format by pre-push hook
    
    * revert prettier config
    
    * automatically format by pre-push hook
    
    * test pre-push
    
    * run prettier for check only
    
    * test pre-push
    
    * fix pre-push hook
    
    * automatically format by pre-push hook
    
    * write e2e

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,23 +5,23 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
-    'prettier'
+    'prettier',
   ],
   overrides: [
     {
       files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
-      extends: ['plugin:playwright/recommended']
-    }
+      extends: ['plugin:playwright/recommended'],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   rules: {
     semi: ['error', 'always'],
@@ -29,6 +29,6 @@ module.exports = {
     'semi-style': ['error', 'last'],
     'no-extra-semi': 'error',
     'no-unexpected-multiline': 'error',
-    'no-unreachable': 'error'
-  }
+    'no-unreachable': 'error',
+  },
 };
