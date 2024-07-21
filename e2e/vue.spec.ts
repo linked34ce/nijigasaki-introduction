1cd7d8701216e64245c6849475d3a9dc11d7f0dc
hosoyan <9797ouki@gmail.com>
2024-07-21 13:32:34 +0900

    write e2e

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,6 +1,35 @@
 import { test, expect } from '@playwright/test';
+import playwright from 'playwright';
 
-test('visits the app root url', async ({ page }) => {
+test('Icons get unclickable after one of them clicked', async ({ page }) => {
   await page.goto('/');
-  await expect(await page.locator('.circle').all()).toHaveLength(10);
+  const yu = page.getByRole('img', { name: 'icon-yu' });
+  const ayumu = page.getByRole('img', { name: 'icon-ayumu' });
+  const kasumi = page.getByRole('img', { name: 'icon-kasumi' });
+  await expect(yu).toBeVisible();
+  await expect(ayumu).toBeVisible();
+  await expect(kasumi).toBeVisible();
+  await yu.click();
+  try {
+    await yu.click({
+      timeout: 1000,
+    });
+    ayumu.click({
+      timeout: 1000,
+    });
+    kasumi.click({
+      timeout: 1000,
+    });
+  } catch (e) {
+    // eslint-disable-next-line playwright/no-conditional-in-test
+    if (e instanceof playwright.errors.TimeoutError) {
+      console.log('The icons are not clickable.');
+    } else {
+      throw new Error('One or more icons are clickable.');
+    }
+  }
+  // Icons are still there although they cannot be seen
+  await expect(yu).toBeVisible();
+  await expect(ayumu).toBeVisible();
+  await expect(kasumi).toBeVisible();
 });
